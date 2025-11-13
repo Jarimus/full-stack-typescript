b9b1c1ee069b6350dbdaf5d45fb1eec4f390db61
jarimus <jari.n.ahonen@gmail.com>
2025-11-13 17:10:54 +0200

    exercise 9.2 done

---
+++ exerciseCalculator.ts
@@ -0,0 +1,50 @@
+type rating = 1 | 2 | 3
+
+interface analysis {
+  periodLength: number,
+  trainingDays: number,
+  target: number,
+  average: number,
+  success: boolean,
+  rating: rating,
+  ratingDescription: string
+}
+
+function calculateExercises(dailyHours: number[], targetAverageHours: number): analysis {
+  // number of days
+  const periodLength = dailyHours.length
+  // number of training days
+  const trainingDays = dailyHours.filter( n => n != 0).length
+  // the original target average hours
+  const target = targetAverageHours
+  // calculated average
+  const sum = dailyHours.reduce( (t, v) => t + v)
+  const average = sum / periodLength
+  // target reached (boolean as 0 or 1)
+  const success = average >= target
+  // rating (1-3)
+  const r = average / target
+  let ratingDescription: string
+  let rating: rating
+  if (r >= 1) {
+    ratingDescription = "Target reached!"
+    rating = 3
+  } else if (r >= 0.5) {
+    ratingDescription = "Good effort!"
+    rating = 2
+  } else {
+    ratingDescription = "More effort next time!"
+    rating = 1
+  }
+  return {
+    periodLength: periodLength,
+    trainingDays: trainingDays,
+    success: success,
+    rating: rating,
+    ratingDescription: ratingDescription,
+    target: target,
+    average: average,
+  }
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
